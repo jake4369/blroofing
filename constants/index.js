c356b0f6df55e8691595f4da9f96f46a33f0f1fd
Jake Samuels <samuels242@googlemail.com>
2023-10-06 17:30:12 +0100

    Secondary review data

--- constants/index.js
+++ constants/index.js
@@ -21,30 +21,60 @@ export const primaryReviews = [
   },
 ];
 
+export const secondaryReviews = [
+  {
+    text: "Came at the time he said he would, stuck to budget, good clean job, would have no problem advising people to use this company.",
+    author: "barry_736",
+  },
+  {
+    text: "Very quick and efficient repointing. Thorough job done with some helpful advice given. Would recommend.",
+    author: "jesstar_55",
+  },
+  {
+    text: "Fast response, Due to recent storm one rig tile blew off from the roof, gave a very reasonable price for the job. Replaced the tile and also fixed another loose tile. Excellent work, very quick, reasonable price, would recommend.",
+    author: "jom62066",
+  },
+  {
+    text: "Quick response, fair quote and professional, friendly service for gutter repair. Would hire again.",
+    author: "rxs707",
+  },
+  {
+    text: "Fantastic lads who were very polite and completed their job on the same day. A little more work was detected around the chimney which was verified by video call prior to the work being carried out. We’ll pleased overall.",
+    author: "shawslane05",
+  },
+  {
+    text: "Excellent service. Quick response . Best quote job well done. Polite and courteous. Will definitely use again for further jobs. Highly recommended cannot fault, went the extra mile.",
+    author: "martin85606",
+  },
+  {
+    text: "Job done quick and professional by the guys, very responsive. I will further recommend them. Thanks.",
+    author: "constantin44426",
+  },
+  {
+    text: "Work completed to a good standard, very professional and polite, easy to communicate with, would definitely use again.",
+    author: "mr20683",
+  },
+];
+
 export const services = [
   {
     service: "Pitched Roofing",
     text: "Elevate the aesthetics and durability of your home with our expertly crafted pitched roofing solutions, designed to withstand the test of time.",
-    img: "/assets/services/facias-2.png",
   },
   {
     service: "Fascias & Soffits",
     text: "Transform your roofline into an elegant and maintenance-free feature that enhances your home's curb appeal while ensuring proper ventilation.",
-    img: "/assets/services/facias.png",
   },
   {
     service: "Guttering",
     text: "Safeguard your property from water damage with our high-quality guttering systems, expertly installed to channel rainwater away efficiently.",
-    img: "/assets/services/facias.png",
   },
   {
     service: "Repointing",
     text: "Revive the charm of your brick or stone surfaces with our meticulous repointing services, preserving the integrity and beauty of your structure.",
-    img: "/assets/services/facias.png",
   },
   {
     service: "Roofing Repairs",
     text: "Trust our skilled team to swiftly and effectively resolve roofing issues, providing peace of mind and protecting your home from further damage.",
-    img: "/assets/services/facias.png",
   },
 ];
