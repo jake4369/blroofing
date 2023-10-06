e7f09f986570d7514d20ee484c782517e258c5d4
Jake Samuels <samuels242@googlemail.com>
2023-10-06 13:11:46 +0100

    Import Services section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Hero from "@components/Hero/Hero";
 import About from "@components/About/About";
 import Reviews from "@components/Reviews/Reviews";
+import Services from "@components/Services/Services";
 
-import { primaryReviews } from "@constants";
+import { primaryReviews, services } from "@constants";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <About />
       <Reviews reviewClassname="reviews__primary" reviewData={primaryReviews} />
+      <Services services={services} />
     </main>
   );
 };
