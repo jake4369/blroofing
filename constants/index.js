e23c7ebdc8443d2d8d43de0dcacd57fef82aa848
Jake Samuels <samuels242@googlemail.com>
2023-10-06 09:33:08 +0100

    Export primary review data

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const primaryReviews = [
+  {
+    text: "Got the job done quickly and was a very neat job. Impressive",
+    author: "olasoji",
+  },
+  {
+    text: "Great communication, brilliant work carried out. Highly recommended",
+    author: "pete69718pete",
+  },
+  {
+    text: "Made me feel confident with his ability to do the job. Honest and competent.",
+    author: "yelena11987",
+  },
+  {
+    text: "Did what needed to be done and backed work up with photographic evidence.",
+    author: "john20435_70",
+  },
+  {
+    text: "Prompt, personable and Job well done, Will hire LB for future job.",
+    author: "mary21598",
+  },
+];
