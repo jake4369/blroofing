6039210e259fcfc6b90ca2ae15545f20c11c4475
Jake Samuels <samuels242@googlemail.com>
2023-10-06 09:30:24 +0100

    Import primary Review section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Hero from "@components/Hero/Hero";
 import About from "@components/About/About";
+import Reviews from "@components/Reviews/Reviews";
+
+import { reviewSetOne } from "@constants";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <About />
+      <Reviews reviewClassname="reviews__primary" reviewData={reviewSetOne} />
     </main>
   );
 };
