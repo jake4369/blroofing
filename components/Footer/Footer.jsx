79256e4364bd657ce8db044ee3c7cfde49a912dd
Jake Samuels <samuels242@googlemail.com>
2023-10-06 17:54:59 +0100

    Footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import CTAButton from "@components/Shared/CTAButton";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span className="footer__logo-text">
+        <span className="highlight__orange">LB</span> Roofing & Brickwork
+      </span>
+
+      <p>
+        If you have any questions and require any furture information on the
+        services we provide, please do not hestitate to contact us.
+      </p>
+
+      <p>
+        We will{" "}
+        <span className="highlight__orange">beat any written quote</span> you
+        recieve so get in touch for a <br />
+        <strong className="highlight__orange">FREE QUOTE</strong>.
+      </p>
+
+      <CTAButton>Contact Us</CTAButton>
+    </footer>
+  );
+};
+
+export default Footer;
