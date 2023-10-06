bcb0cbe7da392e2e7ce14a28f315c49cf437bfbc
Jake Samuels <samuels242@googlemail.com>
2023-10-06 10:52:17 +0100

    About section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,18 +1,20 @@
 const About = () => {
   return (
     <section className="about-section">
-      <h1>LB Brickwork & Roofing</h1>
+      <h1>
+        <span className="highlight__orange">LB</span> Brickwork & Roofing
+      </h1>
 
       <p>
         With over a decade of experience in the industry, we are your go-to
         roofing professionals. We specialize in top-quality roofing repairs and
-        maintenance, delivering enduring results that exceed expectations. Our
-        reputation is built on customer satisfaction and word-of-mouth
-        referrals. Count on us for premium materials and excellence in every
-        project.
+        maintenance, delivering enduring results that exceed expectations. We
+        pride ourselves on our excellent reputation, built on customer
+        satisfaction and word-of-mouth referrals. Count on us for premium
+        materials and excellence in every project.
       </p>
 
-      <span>Your Trusted Roofing Experts</span>
+      <span className="trusted-experts">Your Trusted Roofing Experts</span>
 
       {/* 
           <p>
