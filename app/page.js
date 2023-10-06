83cde279bd9d8061060c8b08baa895e816715aca
Jake Samuels <samuels242@googlemail.com>
2023-10-06 13:25:25 +0100

    Import Image Divider

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "@components/Hero/Hero";
 import About from "@components/About/About";
 import Reviews from "@components/Reviews/Reviews";
+import ImageDivider from "@components/ImageDivider/ImageDivider";
 import Services from "@components/Services/Services";
 
 import { primaryReviews, services } from "@constants";
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <About />
       <Reviews reviewClassname="reviews__primary" reviewData={primaryReviews} />
+      <ImageDivider img="/assets/misc/roofer.png" />
       <Services services={services} />
     </main>
   );
