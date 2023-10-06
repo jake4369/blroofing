1ccdf1a65bd7e2dd53e5d55c5499a94e5947f31c
Jake Samuels <samuels242@googlemail.com>
2023-10-06 10:56:16 +0100

    Hero section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -2,49 +2,11 @@
 
 import { useEffect, useState } from "react";
 
-// const Hero = () => {
-//   const [slides, setSlides] = useState([
-//     "LB Roofing <br /> & <br /> Brickwork",
-//     "Expert Roofing Services Near You",
-//     "Trustworthy and Experienced Roof Installers",
-//     "Quality Roofing Solutions for Your Home",
-//   ]);
-
-//   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-
-//   useEffect(() => {
-//     const intervalId = setInterval(() => {
-//       // Increment the active slide index, wrapping around to 0 if necessary
-//       setActiveSlideIndex((prevIndex) =>
-//         prevIndex === slides.length - 1 ? 0 : prevIndex + 1
-//       );
-//     }, 5000); // Change slide every 3 seconds (adjust as needed)
-
-//     // Clear the interval when the component unmounts
-//     return () => clearInterval(intervalId);
-//   }, [slides]);
-
-//   return (
-//     <section className="hero-section">
-//       <div className="filter"></div>
-
-//       <div className="hero-section__text-carousel">
-//         <p
-//           className="carousel-text"
-//           dangerouslySetInnerHTML={{ __html: slides[activeSlideIndex] }}
-//         ></p>
-//       </div>
-
-//       <button className="hero-section__btn">Get a Quote</button>
-//     </section>
-//   );
-// };
-
 const Hero = () => {
   const slides = [
     "<span class='highlight__orange'>LB</span> Roofing <br /> & <br /> Brickwork",
     "<span class='highlight__orange'>Expert Roofing Services</span> Near You",
-    "<span class='highlight__orange'>Trustworthy and Experienced</span> Roof Installers",
+    "<span class='highlight__orange'>Trustworthy & Experienced</span> Roof Installers",
     "<span class='highlight__orange'>Quality Roofing Solutions</span> <br /> For Your Home",
   ];
 
