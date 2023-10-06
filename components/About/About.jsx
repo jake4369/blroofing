ad34482665f2177ffc42aaa59a2fff5c6e1e7b62
Jake Samuels <samuels242@googlemail.com>
2023-10-06 17:28:34 +0100

    About section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -5,6 +5,8 @@ const About = () => {
         <span className="highlight__orange">LB</span> Brickwork & Roofing
       </h1>
 
+      <span className="trusted-experts">Your Trusted Roofing Experts</span>
+
       <p>
         With over a decade of experience in the industry, we are your go-to
         roofing professionals. We specialize in top-quality roofing repairs and
@@ -13,27 +15,6 @@ const About = () => {
         satisfaction and word-of-mouth referrals. Count on us for premium
         materials and excellence in every project.
       </p>
-
-      <span className="trusted-experts">Your Trusted Roofing Experts</span>
-
-      {/* 
-          <p>
-        <strong>LB Brickwork & Roofing</strong> is a distinguished and trusted
-        roofing company, distinguished by our unparalleled expertise in the
-        field. With a rich history of over a decade in the industry, we are
-        dedicated to delivering top-notch roofing repair and maintenance
-        services, ensuring that every customer receives an exceptional and
-        long-lasting solution.{" "}
-      </p>
-
-      <p>
-        Our pride lies in the fact that a significant portion of our clientele
-        is a result of satisfied customers and referrals through word of mouth.
-        At <strong>LB Brickwork & Roofing</strong>, we are committed to using
-        only the finest quality materials for our projects, ensuring excellence
-        in every aspect of our work.
-      </p>
-      */}
     </section>
   );
 };
