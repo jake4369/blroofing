70916d4cb65fdbc681409f8469c7804c359ac4bf
Jake Samuels <samuels242@googlemail.com>
2023-10-07 14:22:02 +0100

    Import and use ModelContext

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
+"use client";
+
+import { useContext } from "react";
+
 import Hero from "@components/Hero/Hero";
 import About from "@components/About/About";
 import Reviews from "@components/Reviews/Reviews";
 import ImageDivider from "@components/ImageDivider/ImageDivider";
 import Services from "@components/Services/Services";
 import Testimonial from "@components/Testimonial/Testimonial";
+import Modal from "@components/Modal/Modal";
+
+import { ModalContext } from "@context/ModelContext";
 
 import { primaryReviews, services } from "@constants";
 
 const Home = () => {
+  const { modalOpen } = useContext(ModalContext);
   return (
     <main id="home">
       <Hero />
@@ -17,6 +25,7 @@ const Home = () => {
       <Services services={services} />
       <ImageDivider img="/assets/misc/guttering.png" />
       <Testimonial />
+      {modalOpen && <Modal />}
     </main>
   );
 };
