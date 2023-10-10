d0fe8cfdbe16bb9b117a5c41666b9002143c1fa6
Jake Samuels <samuels242@googlemail.com>
2023-10-10 14:03:57 +0100

    Add Head meta data

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import "@styles/index.css";
 
+import Head from "next/head";
+
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
 
 import { ModalProvider } from "@context/ModelContext";
 
 export const metadata = {
-  title: "LB Roofing",
+  title: "LB Roofing - West Midlands",
   description:
     "Trust LB Roofing: Expert West Midlands roofing repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices.",
   canonicalUrl: "https://lb-roofing.vercel.app/",
@@ -15,6 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>LB Roofing</title>
+        <link
+          rel="canonical"
+          href="https://lb-roofing.vercel.app/"
+          key="canonical"
+        />
+      </Head>
       <body>
         <ModalProvider>
           <Header />
