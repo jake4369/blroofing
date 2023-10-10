d3f707a0aabc872ede2720446d3ee463b2fb6856
Jake Samuels <samuels242@googlemail.com>
2023-10-10 13:54:40 +0100

    Shorten meta description

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import { ModalProvider } from "@context/ModelContext";
 export const metadata = {
   title: "LB Roofing",
   description:
-    "With over a decade of industry experience, LB Roofing is your trusted choice in the West Midlands for top-quality roofing repairs, maintenance, and replacements. We use premium materials and advanced techniques to ensure your roof stands strong against the UK's unpredictable weather. Count on us for enduring results and friendly service at competitive prices.",
+    "Trust LB Roofing: Expert West Midlands roofing repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices.",
 };
 
 export default function RootLayout({ children }) {
