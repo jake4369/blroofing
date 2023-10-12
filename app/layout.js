74f2934696cf7c18cd87a1b22e5693e94e2b1b23
Jake Samuels <samuels242@googlemail.com>
2023-10-12 13:08:27 +0100

    Schema.org data

--- app/layout.js
+++ app/layout.js
@@ -8,45 +8,46 @@ import Footer from "@components/Footer/Footer";
 import { ModalProvider } from "@context/ModelContext";
 
 export const metadata = {
-  title: "LB Roofing - West Midlands",
+  title: "LB Roofing",
   description:
-    "Trust LB Roofing: Expert West Midlands roofing repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices.",
-  canonicalUrl: "https://lb-roofing.vercel.app/",
+    "Trust LB Roofing: Expert West Midlands roofing and guttering repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices.",
+  canonicalUrl: "http://www.lbroofer.co.uk/",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>LB Roofing - West Midlands</title>
+        <title>LB Roofing</title>
         <link
           rel="canonical"
-          href="https://lb-roofing.vercel.app/"
+          href="http://www.lbroofer.co.uk/"
           key="canonical"
         />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:title" content="LB Roofing - West Midlands" />
+
+        <meta property="og:title" content="LB Roofing" />
         <meta
           property="og:description"
-          content="Trust LB Roofing: Expert West Midlands roofing repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices."
+          content="Trust LB Roofing: Expert West Midlands roofing and guttering repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices."
         />
         <meta property="og:image" content="/hero.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:url" content="https://lb-roofing.vercel.app/" />
+        <meta property="og:url" content="http://www.lbroofer.co.uk/" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="LB Roofing" />
         <meta property="og:locale" content="en_GB" />
         <meta property="twitter:image" content="/hero.png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="LB Roofing - West Midlands" />
+        <meta property="twitter:title" content="LB Roofing" />
         <meta
           property="twitter:description"
-          content="Trust LB Roofing: Expert West Midlands roofing repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices."
+          content="Trust LB Roofing: Expert West Midlands roofing and guttering repairs, maintenance, replacements. Quality materials, unbeatable service, competitive prices."
         ></meta>
         <meta
           property="og:article:author"
-          content="https://lb-roofing.vercel.app/"
+          content="http://www.lbroofer.co.uk/"
         />
       </Head>
       <body>
@@ -62,22 +63,22 @@ export default function RootLayout({ children }) {
             "@context": "http://schema.org",
             "@type": "LocalBusiness",
             "name": "LB Roofing",
-            "description": "Expert roofing repairs, maintenance, and replacements in the West Midlands.",
-            "url": "https://lb-roofing.vercel.app/",
-            "logo": "https://lb-roofing.vercel.app/assets/services/house-icon.png",
+            "description": "Expert roofing and guttering repairs, maintenance, and replacements in the West Midlands.",
+            "url": "http://www.lbroofer.co.uk/",
+            "logo": "http://www.lbroofer.co.uk/assets/services/house-icon.png",
             "address": {
               "@type": "PostalAddress",
-              "streetAddress": "123 Main Street",
+              "streetAddress": "Old Fallings Lane",
               "addressLocality": "West Midlands",
-              "postalCode": "12345",
+              "postalCode": "WV108BN",
               "addressCountry": "UK"
             },
-            "telephone": "+123456789",
+            "telephone": "+447946509599",
             "openingHours": "Mo-Fr 08:00-17:00",
             "geo": {
               "@type": "GeoCoordinates",
-              "latitude": "51.12345",
-              "longitude": "-0.12345"
+              "latitude": "52.4751",
+              "longitude": "1.8298"
             }
           }
         `}
