f11b9fde90ac853d7182382626b15fba974d8197
Jake Samuels <samuels242@googlemail.com>
2023-10-16 17:02:51 +0100

    Text Scroller

--- components/TextScroller/TextScroller.jsx
+++ components/TextScroller/TextScroller.jsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react";
 
+import Image from "next/image";
+
 const TextScroller = () => {
   useEffect(() => {
     const scrollers = document.querySelectorAll(".scroller");
@@ -30,17 +32,101 @@ const TextScroller = () => {
     <div className="scroller">
       <ul className="tag-list scroller__inner">
         <li>General Roof Repairs</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>New Roofs & Reroofs</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Emergency Roofing</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Lead Work & Flashing</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Cladding, Fascias & Soffits</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Guttering Systems</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Flat Roofing</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Pitched Roofing</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Felt Roofing</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Chimney Work</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Asbestos Removal</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
         <li>Roof Cleaning</li>
+        <Image
+          src="/assets/services/house-icon.png"
+          alt=""
+          aria-hidden="true"
+          width={50}
+          height={50}
+        />
       </ul>
     </div>
   );
