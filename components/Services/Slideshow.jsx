b63b92f6a329b81998b6ca68d941c1c5464516f3
Jake Samuels <samuels242@googlemail.com>
2023-10-16 16:29:58 +0100

    Change slideshow images to background images

--- components/Services/Slideshow.jsx
+++ components/Services/Slideshow.jsx
@@ -1,11 +1,51 @@
 "use client";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-import Image from "next/image";
+// import Image from "next/image";
+
+// const ImageSlideshow = () => {
+//   const images = [
+//     "/assets/slideshow/roofing.png",
+//     "/assets/slideshow/facias.png",
+//     "/assets/slideshow/guttering.png",
+//     "/assets/slideshow/chimney.png",
+//   ];
+//   const interval = 3000;
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   useEffect(() => {
+//     const slideshowInterval = setInterval(() => {
+//       // Increment the current image index
+//       setCurrentImageIndex((prevIndex) =>
+//         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+//       );
+//     }, interval);
+
+//     return () => {
+//       clearInterval(slideshowInterval);
+//     };
+//   }, [images, interval]);
+
+//   return (
+//     <div className="slideshow">
+//       <Image
+//         src={images[currentImageIndex]}
+//         alt={`Slide ${currentImageIndex + 1}`}
+//         width={100}
+//         height={100}
+//         className="slideshow-img"
+//       />
+//     </div>
+//   );
+// };
+
+// export default ImageSlideshow;
+
+import React, { useState, useEffect } from "react";
 
 const ImageSlideshow = () => {
-  const images = [
+  const backgroundImages = [
     "/assets/slideshow/roofing.png",
     "/assets/slideshow/facias.png",
     "/assets/slideshow/guttering.png",
@@ -18,26 +58,22 @@ const ImageSlideshow = () => {
     const slideshowInterval = setInterval(() => {
       // Increment the current image index
       setCurrentImageIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        prevIndex === backgroundImages.length - 1 ? 0 : prevIndex + 1
       );
     }, interval);
 
     return () => {
       clearInterval(slideshowInterval);
     };
-  }, [images, interval]);
+  }, [backgroundImages, interval]);
 
-  return (
-    <div className="slideshow">
-      <Image
-        src={images[currentImageIndex]}
-        alt={`Slide ${currentImageIndex + 1}`}
-        width={100}
-        height={100}
-        className="slideshow-img"
-      />
-    </div>
-  );
+  const containerStyle = {
+    backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
+  return <div className="slideshow" style={containerStyle}></div>;
 };
 
 export default ImageSlideshow;
